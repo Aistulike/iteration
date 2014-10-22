9a6e43b41a07d66e3a39af9cc5cc77cde9247cda
Aiste Sabonyte <45278@longroad.ac.uk>
2014-10-22 15:16:30 +0100

    Changes made on 22-10-14

---
+++ Iteration_Revision exercises_Task1.py
@@ -0,0 +1,8 @@
+#Aiste Sabonyte
+#22-10-14
+#Iteration_Revision exercises task1
+
+for count in range(10):
+    print("Hello World")
+    
+    
---
+++ Iteration_class exercises_task2.py
@@ -0,0 +1,9 @@
+#Aiste Sabonyte
+#22-10-14
+#Iteration_revision exercises_task2
+
+message = input("Please enter the message:")
+number = int(input("please enter the number of times you want the message being printed:"))
+for count in range(number):
+    print(message)
+    
